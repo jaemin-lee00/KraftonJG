d52ed44747fe94dc113263cd70ceea38b2a5c488
jaemin.lee00 <jemin0994@gmail.com>
2025-01-20 00:49:46 +0900

    Add Week 0 processing up to Render Class 1
    Complete Shader Create

---
+++ Week0/Week0/main.cpp
@@ -0,0 +1,256 @@
+#include <windows.h>
+
+// 여기에 아래 코드를 추가 합니다.
+
+// D3D 사용에 필요한 라이브러리들을 링크합니다.
+#pragma comment(lib, "user32")
+#pragma comment(lib, "d3d11")
+#pragma comment(lib, "d3dcompiler")
+
+// D3D 사용에 필요한 헤더파일들을 포함합니다.
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+class URenderer
+{
+public:
+	// Direct3D 11 장치(Device)와 장치 건텍스트(Device Context) 및 스왑 체인(Swap Chain)을 관리하기 위한 포인터들
+	ID3D11Device* Device = nullptr; // GPU와 통신하기 위한 Direct3D 장치
+	ID3D11DeviceContext* DeviceContext = nullptr; // GPU 명령 실행을 담당하는 컨텍스트
+	IDXGISwapChain* SwapChain = nullptr; // 프레임 버퍼를 교체하는데 사용되는 스왑 체인
+
+	// 렌더링에 필요한 리소스 및 상태를 관리하기 위한 변수들
+	ID3D11Texture2D* FrameBuffer = nullptr; // 화면 출력용 텍스쳐
+	ID3D11RenderTargetView* FrameBufferRTV = nullptr; // 텍스쳐를 렌더 타겟으로 사용하는 뷰
+	ID3D11RasterizerState* RasterizerState = nullptr; // 래스터라이저 상태(컬링, 채우기 모드 등 정의)
+	ID3D11Buffer* ConstantBuffer = nullptr; // 쉐이더에 데이터를 전할하기 위한 상수 버퍼
+
+	FLOAT ClearColor[4] = { 0.025f, 0.025f, 0.0025f, 1.0f }; // 화면을 초기화(clear)할 때 사용할 색상 (RGBA)
+	D3D11_VIEWPORT ViewportInfo; // 렌더링 영역을 정희하는 뷰포트 정보
+
+public:
+	// 렌더러 초기화 함수
+	void Create(HWND hWindow)
+	{
+		// Direct3D 장치 및 스왑 체인 생성
+		CreateDeviceAndSwapChain(hWindow);
+
+		// 프레임 버퍼 생성
+		CreateFrameBuffer();
+
+		// 레스터라이저 상태 생성
+		CreateRasterizerState();
+
+		// depth stencil buffer, blend state 는 이 코드에서는 다루지 않음
+	}
+
+	// Direct3D 장치 및 스왑 체인 생성 함수
+	void CreateDeviceAndSwapChain(HWND hWindow)
+	{
+		// 지원하는 Direct3D 기능 레벨을 정의
+		D3D_FEATURE_LEVEL featurelevels[] = { D3D_FEATURE_LEVEL_11_0 };
+
+		// 스왑 체인 설정 구조체 초기화
+		DXGI_SWAP_CHAIN_DESC swapchaindesc = {};
+		swapchaindesc.BufferDesc.Width = 0; // 창 크기에 맞게 자동으로 설정
+		swapchaindesc.BufferDesc.Height = 0; // 창 크기에 맞게 자동으로 설정
+		swapchaindesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM; // 색상 포맷
+		swapchaindesc.SampleDesc.Count = 1; // 멀티 샘플링 비활성화
+		swapchaindesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 렌더 타겟으로 사용
+		swapchaindesc.BufferCount = 2; // 더블 샘플링
+		swapchaindesc.OutputWindow = hWindow; // 렌더링할 창 핸들
+		swapchaindesc.Windowed = TRUE; // 창 모드
+		swapchaindesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; // 스왑 방식
+
+		// Direct3D 장치와 스왑 체인을 생성
+		D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr,
+			D3D11_CREATE_DEVICE_BGRA_SUPPORT | D3D11_CREATE_DEVICE_DEBUG,
+			featurelevels, ARRAYSIZE(featurelevels), D3D11_SDK_VERSION,
+			&swapchaindesc, &SwapChain, &Device, nullptr, &DeviceContext);
+
+		// 생성된 스왑 체인의 정보 가져오기
+		SwapChain->GetDesc(&swapchaindesc);
+
+		// 뷰포트 정보 설정
+		ViewportInfo = { 0.0f, 0.0f, (float)swapchaindesc.BufferDesc.Width, (float)swapchaindesc.BufferDesc.Height, 0.0f, 1.0f };
+	}
+
+	// Direct3D 장치 및 스왑 체인을 해제하는 함수
+	void ReleaseDeviceAndSwapChain()
+	{
+		if (DeviceContext)
+		{
+			DeviceContext->Flush(); // 남아있는 GPU 명령 실행
+		}
+
+		if (SwapChain)
+		{
+			SwapChain->Release();
+			SwapChain = nullptr;
+		}
+
+		if (Device)
+		{
+			Device->Release();
+			Device = nullptr;
+		}
+
+		if (DeviceContext)
+		{
+			DeviceContext->Release();
+			DeviceContext = nullptr;
+		}
+	}
+
+	// 프레임 버퍼를 생성하는 함수
+	void CreateFrameBuffer()
+	{
+		// 스왑 체인으로부터 백 버퍼 텍스처 가져오기
+		SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&FrameBuffer);
+
+		// 렌더 타겟 뷰 생성
+		D3D11_RENDER_TARGET_VIEW_DESC framebufferRTVdesc = {};
+		framebufferRTVdesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM_SRGB; // 색상 포맷
+		framebufferRTVdesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D; // 2D 텍스처
+
+		Device->CreateRenderTargetView(FrameBuffer, &framebufferRTVdesc, &FrameBufferRTV);
+	}
+
+	// 프레임 버퍼를 해제하는 함수
+	void ReleaseFrameBuffer()
+	{
+		if (FrameBuffer)
+		{
+			FrameBuffer->Release();
+			FrameBuffer = nullptr;
+		}
+
+		if (FrameBufferRTV)
+		{
+			FrameBufferRTV->Release();
+			FrameBufferRTV = nullptr;
+		}
+	}
+
+	// 레스터라이저 상태를 생성하는 함수
+	void CreateRasterizerState()
+	{
+		D3D11_RASTERIZER_DESC rasterizerdesc = {};
+		rasterizerdesc.FillMode = D3D11_FILL_SOLID; // 채우기 모드
+		rasterizerdesc.CullMode = D3D11_CULL_BACK; // 백 페이스 컬링
+
+		Device->CreateRasterizerState(&rasterizerdesc, &RasterizerState);
+	}
+
+	// 레스터라이저 상태를 해제하는 함수
+	void ReleaseRasterizerState()
+	{
+		if (RasterizerState)
+		{
+			RasterizerState->Release();
+			RasterizerState = nullptr;
+		}
+	}
+
+	// 렌더러에 사용된 모든 리소스를 해제하는 함수
+	void Release()
+	{
+		RasterizerState->Release();
+
+		// 렌더 타겟을 초기화
+		DeviceContext->OMSetRenderTargets(0, nullptr, nullptr);
+
+		ReleaseFrameBuffer();
+		ReleaseDeviceAndSwapChain();
+	}
+
+	// 스왑 체인의 백 버퍼와 프론트 버퍼를 교체하여 화면에 출력
+	void SwapBuffer()
+	{
+		SwapChain->Present(1, 0); // 1 : Vsync 활성화, 0 : Vsync 비활성화
+	}
+};
+
+
+// 각종 메세지를 처리할 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		//Signal that the app sould quit
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+
+int	WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	// 윈도우 클래스 이름
+	WCHAR WindowClass[] = L"JungleWindowClass";
+
+	// 윈도우 타이틀바에 표시될 이름
+	WCHAR Title[] = L"Game Tech Lab";
+
+	// 각종 메세지를 처리할 함수인 WndProc의 함수 포인터를 WindowClass 구조체에 넣는다.
+	WNDCLASSW wndclass = { 0,  WndProc, 0, 0, 0, 0, 0, 0, WindowClass };
+
+	// 윈도우 클래스 등록
+	RegisterClassW(&wndclass);
+
+	// 1024 x 1024 크기에 윈도우 생성
+	HWND hWnd = CreateWindowExW(0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 1024, 1024,
+		nullptr, nullptr, hInstance, nullptr);
+
+	// Renderere Class를 생성합니다
+	URenderer renderer;
+
+	// D3D11 생성하는 함수를 호출합니다.
+	renderer.Create(hWnd);
+
+	bool bIsExit = false;
+
+	//각종 생성하는 코드를 여기에 추가합니다.
+
+	// Main Loop (Quit Message가 들어오기 전까지 아래 Loop를 무한히 실행하게 됨)
+	while (bIsExit == false)
+	{
+		MSG msg;
+
+		// 처리할 메세지가 더 이상 없을때 까지 수행
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			// 키 입력 메세지를 번역
+			TranslateMessage(&msg);
+
+			// 메세지를 적절한 윈도우 프로시저에 전달, 메세지가 위에서 등록한 WndProc 으로 전달
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+			{
+				bIsExit = true;
+				break;
+			}
+		}
+
+
+		////////////////////////////////////////////////
+		// 매번 실행되는 코드를 여기에 추가합니다.
+
+		// 현재 화면에 보여지는 버퍼와 그리기 작업을 위한 버퍼를 서로 교환합니다.
+		renderer.SwapBuffer();
+		////////////////////////////////////////////////
+	}
+
+	// 소멸하는 코드를 여기에 추가합니다.
+	renderer.SwapBuffer();
+	
+	return 0;
+}
